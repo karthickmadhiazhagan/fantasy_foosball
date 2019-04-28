75b7a753fecc22551533ac71e0a13b0bcdeedb51
Karthick <karthickmadhiazhagan@gmail.com>
2019-04-28 18:38:25 +0530

    Create matches

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_080029) do
+ActiveRecord::Schema.define(version: 2019_04_28_124809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "matches", force: :cascade do |t|
+    t.integer "winning_team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "firstname"
