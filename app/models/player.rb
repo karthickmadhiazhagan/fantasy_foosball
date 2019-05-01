9faee139ca5a081d4cb3dc12aec06467b28c69f6
Karthick <karthickmadhiazhagan@gmail.com>
2019-05-02 04:12:13 +0530

    Test for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,11 @@
 class Player < ApplicationRecord
   belongs_to :team, optional: true
-  validates_presence_of :name
+  validates_presence_of :lastname, :firstname
+  validate :check_team_availability
+
+  def check_team_availability
+  	if self.team.players.count > 1
+  		errors.add(:base, 'Selected team already have 2 players')
+  	end
+  end
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
-class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class PlayerTest < ActiveSupport::TestCase  
+	test "should not save player without firstname and lastname" do
+	  player = Player.new
+	  assert_not player.save
+	  player.firstname = 'Karthick'
+	  assert_not player.save
+	end
+
+	test "should save player without team" do
+	  player = Player.new(:firstname => 'Rabada', :lastname => 'Chrish')
+	  assert player.save
+	end
+
+	test "should save player to the team having less than 2 players" do
+		player = Player.new(:firstname => 'Jack', :lastname => 'Kalish', :team_id => 1)
+	  	assert player.save
+	end
+
+	test "should not save player to the team having already 2 players" do
+		player = Player.new(:firstname => 'AB', :lastname => 'Deviliers', :team_id => 1)
+	  	assert_not player.save
+	end
 end
